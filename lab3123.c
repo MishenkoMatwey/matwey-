a1bfe102ba1c8b9dda95a1d5033c140a18c66beb
matweymishenko <31630131+matwey1999@users.noreply.github.com>
2017-11-29 21:11:16 +0300

    Add files via upload

---
+++ lab3123.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+int main ()
+{
+	char* text=(char*)malloc(10000*sizeof(char));
+	char c;
+	int i=0,n=0,m=0,len=0,fl=0;
+	while ((c=getchar()) !='\n')
+		{
+		text[i++]=c ;
+		}
+
+	for (i=0;i<10000;i++)
+		{
+		if (text[i] == '.' || text[i] == '?' || text[i] == ';') 
+			{
+				i++;   		 
+				text[i] = '\n';
+			
+			
+			}		    
+                if (text[i]=='\t')
+			{
+				text[i]=0;   
+			}	      
+				
+
+		}
+
+
+
+		
+
+		if (text[i] == '.' || text[i] == '?' || text[i] == ';')	
+			
+		{
+			i=len;
+		if (isalpha(text[i]) ==0 &&  text[i-1] !=' ') 
+			{
+			i=i-len;	
+			printf("%s",text);	
+			} 
+		else 
+		printf("%s",text);
+
+			}
+
+	//	if (fl==0)
+	//{printf("%s",text);}
+         //	else //(fl==1)
+	//		fl=0;
+
+
+
+
+
+
+
+//printf("%s",text);
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
